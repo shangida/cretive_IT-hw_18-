4c7756e13a2373d2123dab66ceb16cbc4afd3854
shangida <shangida240@gmail.com>
2023-10-05 16:02:11 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        // all colors strats
+        colors: {
+          navbarBgColor: '#1C1E53',
+          BtnBoderColor: 'rgba(255, 255, 255, 0.05)',
+          BtnHoverColor: '#FCD980',
+          workBgColor: '#F4F6FC',
+          workBtnTextColor: '#2405F2',
+          projectHoverlyColor: 'rgba(28, 30, 83, 0.42)',
+          project1HoverlyBgColor: 'rgba(28, 30, 83, 0.700)',
+          boderColor: '#ECECF1',
+          freHoverlyColor: 'rgba(28, 30, 83, 0.5)',
+        },
+        //ends
+
+        //all fonts strats
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+        //ends
+      }
+    }
+  }
